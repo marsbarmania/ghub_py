cafd41f3715b6f0f0ead4d128dc554165228658e
mars <mars@MarsBookPro.local>
2013-02-17 17:22:33 +0900

    modified

--- cipher/easy_file_cipher.py
+++ cipher/easy_file_cipher.py
@@ -5,7 +5,8 @@ with open("text.txt","r") as origin_file:
   # ord(): the codepoint of the string
   str_num = (ord(str) for str in stream_raw)
   seed = 255
-  str_con = "".join([chr(str^seed) for str in str_num])
+  # str_con = "".join([chr(str^seed) for str in str_num])
+  str_con = "".join(map(lambda str: chr(str^seed),str_num))
 
   with open('text_encode.txt', 'w') as new_file:
     new_file.write(str_con)
